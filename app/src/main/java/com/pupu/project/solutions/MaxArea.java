8caee40276b30e9d770b91a61667521bef420f28
pupu <857484057@qq.com>
2022-06-09 11:46:56 +0800

    盛最多水容器

---
+++ app/src/main/java/com/pupu/project/solutions/MaxArea.java
@@ -0,0 +1,21 @@
+package com.pupu.project.solutions;
+
+public class MaxArea {
+
+    public int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(area, maxArea);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
