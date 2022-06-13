59910640ac66b5b94fdc0a085067df808a052ad0
pupu <857484057@qq.com>
2022-06-13 11:12:44 +0800

    二叉树遍历

--- app/src/main/java/com/pupu/project/JavaMain.java
+++ app/src/main/java/com/pupu/project/JavaMain.java
@@ -6,8 +6,14 @@ import com.pupu.project.sort.HeapSort;
 import com.pupu.project.sort.QuicklySort;
 import com.pupu.project.topk.FindKthLargest;
 import com.pupu.project.topk.TopKFrequent;
+import com.pupu.project.tree.Traversal;
+import com.pupu.project.tree.TreeNode;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 public class JavaMain {
 
@@ -17,15 +23,16 @@ public class JavaMain {
      * @param args
      */
     public static void main(String[] args) {
-        int[] list = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-        int[] test = {9,1,7,10,4,6,5,6,3};
-        int[] test2 = {1,2,3,4,5,6,7,8};
-        int[] test3 = {3,2,3,1,2,4,5,5,6,7,7,8,2,3,1,1,1,10,11,5,6,2,4,7,8,5,6};
-       // System.out.println(Arrays.toString(test));
-        System.out.println(new BinarySearch().search(test2,5));
+        // System.out.println(Arrays.toString(test));
+        // System.out.println(new BinarySearch().search(initList(),5));
+        String result = listToString(new Traversal().inorderTraversal(initBinaryTree()));
+        System.out.println(result);
     }
 
-    public static String listToString(int[] list) {
+    public static String listToString(List<Integer> list) {
+        if (list==null){
+            return "list is empty!!!";
+        }
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("[");
         for (int item : list) {
@@ -33,5 +40,42 @@ public class JavaMain {
         }
         stringBuilder.append("]");
         return stringBuilder.toString();
+    }
+
+    public static TreeNode initBinaryTree() {
+        int[] elements = new int[]{9, 1, 7, 10,0, 4, 6, 5, 6, 3};
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        for (int i : elements) {
+            if (i==0){
+                queue.offer(null);
+            }else{
+                queue.offer(new TreeNode(i));
+            }
+        }
+        TreeNode root = new TreeNode(33);
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node=null;
+            while (node == null){
+                node=stack.pop();
+            }
+            if (!queue.isEmpty() && node.left == null) {
+                node.left = queue.poll();
+                stack.push(node.left);
+            }
+            if (node.right == null && !queue.isEmpty()) {
+                node.right = queue.poll();
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static int[] initList() {
+        //return new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
+        //return new int[]{9,1,7,10,4,6,5,6,3};
+        //return new int[]{1,2,3,4,5,6,7,8};
+        return new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6, 7, 7, 8, 2, 3, 1, 1, 1, 10, 11, 5, 6, 2, 4, 7, 8, 5, 6};
     }
 }
---
+++ app/src/main/java/com/pupu/project/tree/Traversal.java
@@ -0,0 +1,130 @@
+package com.pupu.project.tree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Traversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        first1(root, list);
+        return list;
+    }
+
+    /**
+     * 中序遍历
+     *
+     * @param node
+     * @param list
+     */
+    private void inorder(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, list);
+        list.add(node.val);
+        inorder(node.right, list);
+    }
+
+    private void inorder1(TreeNode node, List<Integer> list) {
+        Stack<TreeNode> stack = new Stack<>();
+        while (node != null || !stack.isEmpty()) {
+            if (node!=null){
+                stack.push(node);
+                node=node.left;
+            }else {
+                node=stack.pop();
+                list.add(node.val);
+                node=node.right;
+            }
+        }
+    }
+
+    /**
+     * 先序遍历
+     * @param node
+     * @param list
+     */
+    private void first(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        list.add(node.val);
+        first(node.left, list);
+        first(node.right, list);
+    }
+
+    private void first1(TreeNode node, List<Integer> list) {
+        Stack<TreeNode> stack = new Stack<>();
+        while (node != null || !stack.isEmpty()) {
+            if (node != null) {
+                list.add(node.val);
+                stack.push(node);
+                node = node.left;
+            } else {
+                node = stack.pop();
+                node = node.right;
+            }
+        }
+    }
+
+    /**
+     * 后序遍历
+     * @param root
+     * @param list
+     */
+    private void end(TreeNode root,List<Integer> list){
+        if (root==null){
+            return;
+        }
+        end(root.left,list);
+        end(root.right,list);
+        list.add(root.val);
+    }
+
+    private void end1(TreeNode node,List<Integer> list){
+        Stack<TreeNode> stack=new Stack<>();
+        while (node!=null || !stack.isEmpty()){
+            if (node!=null){
+                list.add(node.val);
+                stack.push(node);
+                node=node.right;
+            }else {
+                node=stack.pop();
+                node=node.left;
+            }
+        }
+        Collections.reverse(list);
+    }
+
+
+    /**
+     * 层次遍历
+     * @param node
+     * @return
+     */
+    public List<Integer> layerTraversal(TreeNode node) {
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(node);
+
+        while (!queue.isEmpty()) {
+            TreeNode p = queue.poll();
+            result.add(p.val);
+            if (p.left != null) {
+                queue.offer(p.left);
+            }
+            if (p.right != null) {
+                queue.offer(p.right);
+            }
+
+        }
+
+        return result;
+    }
+}
