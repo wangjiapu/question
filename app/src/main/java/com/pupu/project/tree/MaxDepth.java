afae5d75764e728ca107c37ca18a7805719da170
pupu <857484057@qq.com>
2022-06-13 12:15:05 +0800

    二叉树的最小深度

--- app/src/main/java/com/pupu/project/tree/MaxDepth.java
+++ app/src/main/java/com/pupu/project/tree/MaxDepth.java
@@ -42,4 +42,18 @@ public class MaxDepth {
         }
         return Math.max(maxDepth1(root.left), maxDepth1(root.right)) + 1;
     }
+
+    public int minDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
 }
