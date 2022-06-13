483ca108ec0b8e862df254d3d2cd3ad81239191d
pupu <857484057@qq.com>
2022-06-13 11:48:57 +0800

    二叉树的最大深度

---
+++ app/src/main/java/com/pupu/project/tree/MaxDepth.java
@@ -0,0 +1,45 @@
+package com.pupu.project.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaxDepth {
+    /**
+     * 广度优先
+     *
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+
+    }
+
+    public int maxDepth1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth1(root.left), maxDepth1(root.right)) + 1;
+    }
+}
