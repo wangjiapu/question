454a7a8e275f3464e3c6fef150c8a407c0aaebe8
pupu <857484057@qq.com>
2022-06-17 18:45:31 +0800

    3. 无重复字符的最长子串

--- app/src/main/java/com/pupu/project/solutions/SubString.java
+++ app/src/main/java/com/pupu/project/solutions/SubString.java
@@ -1,9 +1,14 @@
 package com.pupu.project.solutions;
 
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -35,9 +40,36 @@ public class SubString {
         int left = 0;
         for (int right = 0; right < s.length(); right++) {
             left = Math.max(m[s.charAt(right)], left);
-            m[s.charAt(right)] = right+1;
-            max = Math.max(max, right - left +1);
+            m[s.charAt(right)] = right + 1;
+            max = Math.max(max, right - left + 1);
         }
         return max;
     }
+
+    /**
+     * 3. 无重复字符的最长子串
+     * @param s
+     * @return
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public int lengthOfLongestSubstring2(String s) {
+        int res = 0;
+        Map<Character, Integer> window = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        while (right < s.length()) {
+            char ch = s.charAt(right);
+            right++;
+            window.put(ch, window.getOrDefault(ch, 0) + 1);
+
+            while (window.get(ch) > 1) {
+                char d = s.charAt(left);
+                left++;
+                window.put(d, window.getOrDefault(d, 0) - 1);
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
+
 }
