3a7d4435b21d304371c2aa1d1fd6fb7c6c36b1cd
pupu <857484057@qq.com>
2022-06-20 10:04:36 +0800

    23. 合并K个升序链表

--- app/src/main/java/com/pupu/project/lists/MergeTwoLists.java
+++ app/src/main/java/com/pupu/project/lists/MergeTwoLists.java
@@ -1,5 +1,8 @@
 package com.pupu.project.lists;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MergeTwoLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(0);
@@ -16,5 +19,39 @@ public class MergeTwoLists {
         }
         dummy.next = list1 != null ? list1 : list2;
         return newHead.next;
+    }
+
+    /**
+     * 23. 合并K个升序链表
+     * @param lists
+     * @return
+     */
+    ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+        for (ListNode node : lists) {
+            if (node != null) {
+                queue.offer(node);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            ListNode minNode = queue.poll();
+            p.next = minNode;
+            if (minNode.next != null) {
+                queue.offer(minNode.next);
+            }
+            p = p.next;
+        }
+        return dummy.next;
     }
 }
