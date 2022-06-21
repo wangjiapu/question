90293fa47811ce422483def174d2c66642e9a469
pupu <857484057@qq.com>
2022-06-21 11:02:06 +0800

    83. 删除排序链表中的重复元素

--- app/src/main/java/com/pupu/project/solutions/RemoveDuplicates.java
+++ app/src/main/java/com/pupu/project/solutions/RemoveDuplicates.java
@@ -1,8 +1,11 @@
 package com.pupu.project.solutions;
 
+import com.pupu.project.lists.ListNode;
+
 public class RemoveDuplicates {
     /**
      * 26. 删除有序数组中的重复项
+     *
      * @param nums
      * @return
      */
@@ -15,5 +18,28 @@ public class RemoveDuplicates {
             }
         }
         return left + 1;
+    }
+
+    /**
+     * 83. 删除排序链表中的重复元素
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode left = head;
+        ListNode right = head;
+        while (right != null) {
+            if (left.val < right.val) {
+                left.next = right;
+                left = left.next;
+            }
+            right = right.next;
+        }
+        // 断开与后面重复元素的连接
+        left.next = null;
+        return head;
     }
 }
