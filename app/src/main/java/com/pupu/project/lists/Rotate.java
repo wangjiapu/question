e84525b4e9e332a573e6acdf177354c61776ca75
pupu <857484057@qq.com>
2022-06-22 12:29:54 +0800

    54. 螺旋矩阵

--- app/src/main/java/com/pupu/project/lists/Rotate.java
+++ app/src/main/java/com/pupu/project/lists/Rotate.java
@@ -1,5 +1,8 @@
 package com.pupu.project.lists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rotate {
     // 将二维矩阵原地顺时针旋转 90 度
     public void rotate(int[][] matrix) {
@@ -30,5 +33,50 @@ public class Rotate {
             i++;
             j--;
         }
+    }
+
+    /**
+     * 54. 螺旋矩阵
+     *
+     * @param matrix
+     * @return
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int top = 0;
+        int left = 0;
+        int bottom = m - 1;
+        int right = n - 1;
+        List<Integer> res = new ArrayList<>();
+
+        while (res.size() < m * n) {
+            if (top <= bottom) {
+                for (int i = left; i <= right; i++) {
+                    res.add(matrix[top][i]);
+                }
+                top++;
+            }
+            if (left <= right) {
+                for (int i = top; i <= bottom; i++) {
+                    res.add(matrix[i][right]);
+                }
+                right--;
+            }
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return res;
     }
 }
