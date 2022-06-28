a7f2c7770bf0e2bf1810e85a5bec567e088565fc
pupu <857484057@qq.com>
2022-06-28 11:04:33 +0800

    543. 二叉树的直径

---
+++ app/src/main/java/com/pupu/project/tree/DiameterOfBinaryTree.java
@@ -0,0 +1,28 @@
+package com.pupu.project.tree;
+
+public class DiameterOfBinaryTree {
+    int maxDiameter = 0;
+
+    /**
+     * 543. 二叉树的直径
+     * @param root
+     * @return
+     */
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return maxDiameter;
+    }
+
+    int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+        // 后序位置，顺便计算最大直径
+        int myDiameter = leftMax + rightMax;
+        maxDiameter = Math.max(maxDiameter, myDiameter);
+
+        return 1 + Math.max(leftMax, rightMax);
+    }
+}
