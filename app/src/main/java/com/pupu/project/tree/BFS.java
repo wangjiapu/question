45b242ad1b9952035c99bb315dc08bd069c48781
pupu <857484057@qq.com>
2022-06-28 13:46:27 +0800

    752. 打开转盘锁[BFS]

---
+++ app/src/main/java/com/pupu/project/tree/BFS.java
@@ -0,0 +1,135 @@
+package com.pupu.project.tree;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class BFS {
+
+    //模板
+    // 计算从起点 start 到终点 target 的最近距离
+//    int BFS(Node start, Node target) {
+//        Queue<Node> q; // 核心数据结构
+//        Set<Node> visited; // 避免走回头路
+//
+//        q.offer(start); // 将起点加入队列
+//        visited.add(start);
+//        int step = 0; // 记录扩散的步数
+//
+//        while (q not empty) {
+//            int sz = q.size();
+//            /* 将当前队列中的所有节点向四周扩散 */
+//            for (int i = 0; i < sz; i++) {
+//                Node cur = q.poll();
+//                /* 划重点：这里判断是否到达终点 */
+//                if (cur is target)
+//                return step;
+//                /* 将 cur 的相邻节点加入队列 */
+//                for (Node x : cur.adj())
+//                    if (x not in visited) {
+//                    q.offer(x);
+//                    visited.add(x);
+//                }
+//            }
+//            /* 划重点：更新步数在这里 */
+//            step++;
+//        }
+//    }
+
+
+    /**
+     * 二叉树的最小深度
+     *
+     * @param root
+     * @return
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int depth = 1;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+
+    /**
+     * 752. 打开转盘锁
+     * @param deadends
+     * @param target
+     * @return
+     */
+    public int openLock(String[] deadends, String target) {
+        Queue<String> queue = new LinkedList<>();
+        Set<String> deads = new HashSet<>(Arrays.asList(deadends));
+        Set<String> visited = new HashSet<>();
+        queue.offer("0000");
+        int step = 0;
+        visited.add("0000");
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String s = queue.poll();
+                if (deads.contains(s)) {
+                    continue;
+                }
+                if (s.equals(target)) {
+                    return step;
+                }
+                for (int j = 0; j < 4; j++) {
+                    String up = plusOne(s, j);
+                    if (!visited.contains(up)) {
+                        queue.offer(up);
+                        visited.add(up);
+                    }
+                    String dowm = downOne(s, j);
+                    if (!visited.contains(dowm)) {
+                        queue.offer(dowm);
+                        visited.add(dowm);
+                    }
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+
+    String plusOne(String s, int j) {
+        char[] chars = s.toCharArray();
+        if (chars[j] == '9') {
+            chars[j] = '0';
+        } else {
+            chars[j] += 1;
+        }
+        return new String(chars);
+    }
+
+    String downOne(String s, int j) {
+        char[] chars = s.toCharArray();
+        if (chars[j] == '0') {
+            chars[j] = '9';
+        } else {
+            chars[j] -= 1;
+        }
+        return new String(chars);
+    }
+}
