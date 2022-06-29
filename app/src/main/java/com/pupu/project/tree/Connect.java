76abbdadbfd202ce6cbb81c561102dad49fba15c
pupu <857484057@qq.com>
2022-06-29 11:19:50 +0800

    114. 二叉树展开为链表

--- app/src/main/java/com/pupu/project/tree/Connect.java
+++ app/src/main/java/com/pupu/project/tree/Connect.java
@@ -3,24 +3,46 @@ package com.pupu.project.tree;
 public class Connect {
     /**
      * 116. 填充每个节点的下一个右侧节点指针
+     *
      * @param root
      * @return
      */
     public Node connect(Node root) {
-        if (root==null){
+        if (root == null) {
             return null;
         }
-        traverse(root.left,root.right);
+        traverse(root.left, root.right);
         return root;
     }
 
-    private void traverse(Node node1,Node node2){
-        if (node1==null||node2==null){
+    private void traverse(Node node1, Node node2) {
+        if (node1 == null || node2 == null) {
             return;
         }
-        node1.next=node2;
-        traverse(node1.left,node1.right);
-        traverse(node2.left,node2.right);
-        traverse(node1.right,node2.left);
+        node1.next = node2;
+        traverse(node1.left, node1.right);
+        traverse(node2.left, node2.right);
+        traverse(node1.right, node2.left);
+    }
+
+    /**
+     * 114. 二叉树展开为链表
+     * @param root
+     */
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = null;
+        root.right = left;
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = right;
     }
 }
