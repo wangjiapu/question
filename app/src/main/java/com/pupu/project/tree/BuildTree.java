3ac7dc92ae627433f02dfda19c6ebe32f9ed7f96
pupu <857484057@qq.com>
2022-07-01 11:54:48 +0800

    106. 从中序与后序遍历序列构造二叉树

--- app/src/main/java/com/pupu/project/tree/BuildTree.java
+++ app/src/main/java/com/pupu/project/tree/BuildTree.java
@@ -8,6 +8,7 @@ public class BuildTree {
 
     /**
      * 105. 从前序与中序遍历序列构造二叉树
+     *
      * @param preorder
      * @param inorder
      * @return
@@ -31,6 +32,35 @@ public class BuildTree {
 
         node.left = build(pre, preStart + 1, preStart + range, inoder, inorderStart, rootIndex - 1);
         node.right = build(pre, preStart + range + 1, preEnd, inoder, rootIndex + 1, inorderEnd);
+        return node;
+    }
+
+    /**
+     * 106. 从中序与后序遍历序列构造二叉树
+     *
+     * @param inorder
+     * @param postorder
+     * @return
+     */
+    public TreeNode buildTree1(int[] inorder, int[] postorder) {
+        inorderMap = new HashMap<>(inorder.length);
+        for (int i = 0; i < inorder.length; ++i) {
+            inorderMap.put(inorder[i], i);
+        }
+        return build1(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
+
+    private TreeNode build1(int[] inorder, int inorderStart, int inorderEnd, int[] postorder, int postorderStart, int postorderEnd) {
+
+        if (inorderStart > inorderEnd) {
+            return null;
+        }
+        TreeNode node = new TreeNode(postorder[postorderEnd]);
+        int index = inorderMap.get(postorder[postorderEnd]);
+
+        int range = index - inorderStart;
+        node.left = build1(inorder, inorderStart, index - 1, postorder, postorderStart, postorderStart + range - 1);
+        node.right = build1(inorder, index + 1, inorderEnd, postorder, postorderStart + range, postorderEnd - 1);
         return node;
     }
 }
