a66b6fcbb228d6a3786bea3cd37e6c9eb9eb183b
pupu <857484057@qq.com>
2022-08-06 11:01:57 +0800

    88. 合并两个有序数组

---
+++ app/src/main/java/com/pupu/project/solutions/Merge.java
@@ -0,0 +1,14 @@
+package com.pupu.project.solutions;
+
+import java.util.Arrays;
+
+public class Merge {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int end = m + n - 1;
+        while (j >= 0) {
+            nums1[end--] = (i >= 0 && nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
+        }
+    }
+}
